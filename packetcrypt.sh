9e88596f8627485806dc52cea5a18e369077f4d4
nxjaksk <97390621+nxjaksk@users.noreply.github.com>
2022-01-20 19:11:12 +0700

    Update packetcrypt.sh

--- packetcrypt.sh
+++ packetcrypt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.4.4/packetcrypt-v0.4.4-linux_amd64
 chmod +x packetcrypt-v0.4.4-linux_amd64
-./packetcrypt-v0.4.4-linux_amd64 ann -p pkt1qjkvy96yk4hy4uuftrpncf8duekzzfrxha44ehl http://pool.pktpool.io
+./packetcrypt-v0.4.4-linux_amd64 ann -p pkt1qsdsw62r5aardmqahq0syp5kltaa78ue7yqpwmx http://pool.pkt.world http://pool.pktpool.io
